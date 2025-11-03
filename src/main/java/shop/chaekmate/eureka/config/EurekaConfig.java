702c8da080f78eedcd295bc2e9afbd10d944b3ca
chaewonn <scw0629@naver.com>
2025-11-03 17:04:30 +0900

    refactor: Eureka Config 분리

---
+++ src/main/java/shop/chaekmate/eureka/config/EurekaConfig.java
@@ -0,0 +1,10 @@
+package shop.chaekmate.eureka.config;
+
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.Configuration;
+
+@EnableEurekaServer
+@Configuration
+public class EurekaConfig {
+
+}
