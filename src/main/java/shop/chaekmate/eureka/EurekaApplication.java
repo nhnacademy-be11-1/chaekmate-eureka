385dce0d7865ab926543f6e3a53875ff7bb2518d
chaewonn <scw0629@naver.com>
2025-11-05 11:14:37 +0900

    Feat/setting : Gateway & Eureka 환경구성 (#2)
    * feat: 유레카 초기 세팅
    
    * chore: 환경변수 세팅, jacoco 관련 세팅
    
    * chore: 주석제거
    
    * chore: PR 두레이 알림 추가
    
    * chore: gateway 및 eureka 연결
    
    * Eureka 설정 완료
    
    * chore: application.yml management 설정 추가
    
    * refactor: Eureka Config 분리
    
    ---------
    
    Co-authored-by: chzhxkd100 <chzhxkd100@naver.com>

--- src/main/java/shop/chaekmate/eureka/EurekaApplication.java
+++ src/main/java/shop/chaekmate/eureka/EurekaApplication.java
@@ -2,6 +2,7 @@ package shop.chaekmate.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 public class EurekaApplication {
