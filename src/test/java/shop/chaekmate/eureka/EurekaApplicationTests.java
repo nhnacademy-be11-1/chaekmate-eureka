2e7156dbacac7ccfd2fc11b3e91c5f263c3a92e2
chzhxkd100 <chzhxkd100@naver.com>
2025-11-07 16:35:54 +0900

    fix: 테스트 프로파일 추가

--- src/test/java/shop/chaekmate/eureka/EurekaApplicationTests.java
+++ src/test/java/shop/chaekmate/eureka/EurekaApplicationTests.java
@@ -2,8 +2,10 @@ package shop.chaekmate.eureka;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class EurekaApplicationTests {
 
 	@Test
